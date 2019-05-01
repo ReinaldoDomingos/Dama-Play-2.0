e131a5c35957e3a8c4c495e0e3454abb79cd03da
ReinaldoDomingos <reijunior100@gmail.com>
2019-05-01 19:31:39 -0400

    Tabuleiro Completo Preenchido com as peças de 4 x 4 ou superior

--- js/main.js
+++ js/main.js
@@ -1,13 +1,10 @@
 angular.module('app', []);
 angular.module('app').controller('scripts',
         function ($scope) {
-            var tabuleiro = new Tabuleiro(5);
+            $scope.tamanho = 6;
+            var tabuleiro = new Tabuleiro($scope.tamanho);
             $scope.tabuleiro = [];
             var pecas = tabuleiro.getPecas();
             $scope.tabuleiro = pecas;
-//            console.log($scope.pecas[0].linha.length);
-//            console.log($scope.pecas[1].linha.length);
-//            console.log($scope.pecas[2].linha.length);
-//            console.log($scope.pecas[3].linha.length);
         }
 );
