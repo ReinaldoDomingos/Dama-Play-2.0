6343e87bd73f218f50b7bc6d188e0ffec4be0361
ReinaldoDomingos <reijunior100@gmail.com>
2019-05-01 19:31:39 -0400

    Adicionar metodo selecionar para peça

--- js/main.js
+++ js/main.js
@@ -1,13 +1,26 @@
 angular.module('app', []);
 angular.module('app').controller('scripts',
         function ($scope) {
-            var tabuleiro = new Tabuleiro(5);
+            $scope.tamanho = 6;
+            var tabuleiro = new Tabuleiro($scope.tamanho);
             $scope.tabuleiro = [];
             var pecas = tabuleiro.getPecas();
             $scope.tabuleiro = pecas;
-//            console.log($scope.pecas[0].linha.length);
-//            console.log($scope.pecas[1].linha.length);
-//            console.log($scope.pecas[2].linha.length);
-//            console.log($scope.pecas[3].linha.length);
+            $scope.selecionar = function (peca) {
+                console.log(peca.selecionado);
+                if (!peca.selecionado) {
+                    peca.selecionado = true;
+                    console.log(peca.selecionado);
+                    console.log(peca);
+                    for (var i = 0; i < this.tamanho * 2; i++) {
+                        for (var j = 0; j < this.tamanho * 2; j++) {
+                            if (peca !== $scope.tabuleiro[i][j])
+                                $scope.tabuleiro[i][j].selecionado = false;
+                        }
+                    }
+
+                }
+            };
+
         }
 );
--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,22 @@
+var n = 0;
+$(document).ready(function () {
+    var w = $("button").css('width');
+    w = w.substring(0, w.indexOf('px'));
+    if (n == 5)
+        $("button").css('width', (w * 1.5) + "px");
+    else if (n == 4)
+        $("button").css('width', (w * 2) + "px");
+    $('.decimo').css('height', $("button").css('width'));
+    $('button').css('height', $("button").css('width'));
+//    $('button').css('width', $(".decimo").css('width'));
+//    var h = $(".decimo").css('width');
+//    h = h.substring(0, h.indexOf('px'));
+//    $('.jumbotron').css('height', h + 'px');
+});
 class  Tabuleiro {
     constructor(tamanho) {
         this.tamanho = tamanho;
+        n = this.tamanho;
         this.pecas = [];
         for (var i = 0; i < this.tamanho; i++) {
             this.pecas[i] = [];
@@ -15,15 +31,33 @@ class  Tabuleiro {
         return this.pecas;
     }
     preencherPecas() {
-        for (var i = 0; i < this.tamanho; i++) {
+        for (var i = 0; i < this.tamanho * 2; i++) {
             var colunas = [];
-            for (var j = 0; j < this.tamanho; j++) {
-                if (i % 2 == 0 && j % 2 == 0)
-                    colunas[j] = "secundary";
-                else
-                    colunas[j] = "primary";
+            for (var j = 0; j < this.tamanho * 2; j++) {
+                var img;
+                if ((i % 2 == 0 & j % 2 == 0) || (i % 2 != 0 & j % 2 != 0)) {
+                    if (i <= n - 2) {
+                        img = 'url(img/peca_jogador_1.png)'
+                    } else if (i > n) {
+                        img = 'url(img/peca_jogador_2.png)';
+                    } else {
+                        img = 'url(img/espaco_casa.png)';
+                    }
+                    colunas[j] = {
+                        cor: "primary",
+                        img: img,
+                        selecionado: false
+                    }
+                } else {
+                    colunas[j] = {
+                        cor: "secundary",
+                        img: 'url(img/espaco_vazio.png)',
+                        selecionado: false
+                    };
+//                    (i % 2 != 0 && j % 2 == 0)
+                }
             }
-            this.pecas[i].linha = colunas;
+            this.pecas[i] = colunas;
         }
     }
 }
