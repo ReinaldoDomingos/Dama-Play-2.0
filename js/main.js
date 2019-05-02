dd68a0d9e6055181c59a26b7875a071603ae38a8
ReinaldoDomingos <reijunior100@gmail.com>
2019-05-02 00:48:04 -0400

    Merge origin/master_tabuleiro into master_tabuleiro
    Conflicts:
    	index.html
    	js/main.js
    	js/scripts.js

--- js/main.js
+++ js/main.js
@@ -1,26 +1,64 @@
 angular.module('app', []);
 angular.module('app').controller('scripts',
         function ($scope) {
-            $scope.tamanho = 6;
+            $scope.tamanho = 4;
             var tabuleiro = new Tabuleiro($scope.tamanho);
             $scope.tabuleiro = [];
             var pecas = tabuleiro.getPecas();
             $scope.tabuleiro = pecas;
+            $scope.altura = '34px';
+            $scope.peca1 = '';
+            $scope.peca2 = '';
+            $scope.iniciar = function () {
+                $(document).ready(function () {
+                    getAltura();
+                });
+            };
+            $scope.iniciar();
             $scope.selecionar = function (peca) {
-                console.log(peca.selecionado);
-                if (!peca.selecionado) {
-                    peca.selecionado = true;
-                    console.log(peca.selecionado);
-                    console.log(peca);
-                    for (var i = 0; i < this.tamanho * 2; i++) {
-                        for (var j = 0; j < this.tamanho * 2; j++) {
-                            if (peca !== $scope.tabuleiro[i][j])
-                                $scope.tabuleiro[i][j].selecionado = false;
+                $scope.altura = '34px';
+//                console.log(peca);
+//                console.log(!peca.selecionada);
+//                console.log((peca.tipo !== 'casa'));
+                console.log($scope.peca1 !== '');
+                if (!peca.selecionada && (peca.tipo !== 'casa' || $scope.peca1 !== '')) {
+                    var qtdSelecionada = tabuleiro.getQtdSeleciondaENaoOcupada();
+                    console.log(qtdSelecionada);
+                    if (qtdSelecionada == 1) {
+                        console.log('a2');
+                        peca.selecionada = true;
+                        $scope.peca2 = peca;
+//                        var outraPeca = tabuleiro.getPecaSelecionda(peca);
+//                        console.log(outraPeca);
+//                        trocarPecas(peca, outraPeca);
+                        var peca1 = $scope.peca1;
+                        var peca2 = $scope.peca2;
+                        console.log(peca1.ocupada);
+                        console.log(!peca2.ocupada);
+                        if (peca1.ocupada && !peca2.ocupada) {
+                            console.log("ok");
+                            trocarPecas(peca1, peca2);
+                            console.log('ok');
+                            $scope.peca1 = '';
+                            $scope.peca2 = '';
+                        }
+                        tabuleiro.resetPecas();
+                        $(document).ready(function () {
+                            $scope.altura = getAltura();
+//                            console.log($scope.altura);
+                        });
+                    } else {
+                        if (peca.tipo !== 'vazio' && peca.tipo !== 'casa') {
+                            peca.selecionada = true;
+                            $scope.peca1 = peca;
+                            console.log("a1");
                         }
                     }
-
+                } else {
+                    peca.selecionada = false;
+                    console.log('a3');
                 }
-            };
-
+            }
+            ;
         }
 );
--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,40 @@
-var n = 0;
-$(document).ready(function () {
-    var w = $("button").css('width');
-    w = w.substring(0, w.indexOf('px'));
-    if (n == 5)
-        $("button").css('width', (w * 1.5) + "px");
-    else if (n == 4)
-        $("button").css('width', (w * 2) + "px");
-    $('.decimo').css('height', $("button").css('width'));
-    $('button').css('height', $("button").css('width'));
-//    $('button').css('width', $(".decimo").css('width'));
-//    var h = $(".decimo").css('width');
-//    h = h.substring(0, h.indexOf('px'));
-//    $('.jumbotron').css('height', h + 'px');
-});
+var n = 0, ex = 0, w = 0;
+
+function trocarPecas(peca, outraPeca) {
+    var aux = peca.img;
+//    var aux2 = peca.selecionada;
+    var aux3 = peca.ocupada;
+    console.log(aux3);
+    peca.img = outraPeca.img;
+//    peca.selecionada = outraPeca.selecionada;
+    peca.ocupada = outraPeca.ocupada;
+    console.log(peca.ocupada);
+    outraPeca.img = aux;
+//    outraPeca.selecionada = aux2;
+    outraPeca.ocupada = aux3;
+//    peca.selecionada = false;
+//    outraPeca.selecionada = false;
+}
+
+function getAltura() {
+    if (w == 0) {
+        ex = 1;
+        w = $("button").css('width');
+        w = w.substring(0, w.indexOf('px'));
+        if (n == 5)
+            $("button").css('width', (w * 1.5) + "px");
+        else if (n == 4)
+            $("button").css('width', (w * 2) + "px");
+        $('button').css('height', $("button").css('width'));
+        w = $("button").css('width');
+        var h = $(".decimo").css('width');
+        h = h.substring(0, h.indexOf('px'));
+        return w;
+    } else {
+        $("button").css('width', w);
+        $("button").css('height', w);
+    }
+}
 class  Tabuleiro {
     constructor(tamanho) {
         this.tamanho = tamanho;
@@ -30,36 +52,78 @@ class  Tabuleiro {
 //        this.setImagePecas();
         return this.pecas;
     }
+    setPecas(pecas) {
+        this.pecas = pecas;
+    }
     preencherPecas() {
         for (var i = 0; i < this.tamanho * 2; i++) {
             var colunas = [];
             for (var j = 0; j < this.tamanho * 2; j++) {
-                var img;
+                var img, ocupada = false, tipo = 'vazio';
                 if ((i % 2 == 0 & j % 2 == 0) || (i % 2 != 0 & j % 2 != 0)) {
                     if (i <= n - 2) {
-                        img = 'url(img/peca_jogador_1.png)'
+                        img = 'url(img/peca_jogador_1.png)';
+                        ocupada = true;
+                        tipo = 'jogador1';
                     } else if (i > n) {
                         img = 'url(img/peca_jogador_2.png)';
+                        tipo = 'jogador2';
+                        ocupada = true;
                     } else {
                         img = 'url(img/espaco_casa.png)';
+                        tipo = 'casa';
                     }
+//                    console.log(img);
                     colunas[j] = {
+                        id: '-' + i + '-' + j,
                         cor: "primary",
                         img: img,
                         selecionada: false,
-                        ocupada: false
+                        ocupada: ocupada,
+                        tipo: tipo
                     }
                 } else {
+                    ocupada = true;
                     colunas[j] = {
+                        id: '-' + i + '-' + j,
                         cor: "secundary",
                         img: 'url(img/espaco_vazio.png)',
                         selecionada: false,
-                        ocupada: false
+                        ocupada: ocupada,
+                        tipo: tipo
                     };
 //                    (i % 2 != 0 && j % 2 == 0)
                 }
             }
             this.pecas[i] = colunas;
+        }
+//            console.log(this.pecas);
+    }
+    getQtdSeleciondaENaoOcupada(peca) {
+        var qtdSelecionada = 0;
+        for (var i = 0; i < this.tamanho * 2; i++) {
+            for (var j = 0; j < this.tamanho * 2; j++) {
+                if (this.pecas[i][j].selecionada &&
+                        this.pecas[i][j].tipo !== 'vazio' && this.pecas[i][j] !== peca)
+                    qtdSelecionada++;
+            }
+        }
+        return qtdSelecionada;
+    }
+    getPecaSelecionda(peca) {
+        for (var i = 0; i < this.tamanho * 2; i++) {
+            for (var j = 0; j < this.tamanho * 2; j++) {
+                if (this.pecas[i][j].selecionada && this.pecas[i][j] !== peca) {
+                    return  this.pecas[i][j];
+                }
+            }
+        }
+    }
+    resetPecas() {
+        for (var i = 0; i < this.tamanho * 2; i++) {
+            for (var j = 0; j < this.tamanho * 2; j++) {
+                this.pecas[i][j].selecionada = false;
+            }
         }
     }
 }
