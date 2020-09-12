d740e7eecfd83c482da1fb3aa6eed23e097cc13d
ReinaldoDomingos <reijunior100@gmail.com>
2020-09-12 12:34:15 -0400

    Mudando para let e ===. E removendo código não ultilizado.

--- js/main.js
+++ js/main.js
@@ -1,15 +1,16 @@
 $('.hide').hide();
-var ultimaMexida = '', iniciou = 0;
-var p1, p2, tabuleiros;
+let ultimaMexida = '', iniciou = 0;
+let p1, p2, tabuleiros;
+
 angular.module('app', []);
+
 angular.module('app').controller('scripts',
         function ($scope) {
             $scope.tamanho = 4;
-            var tabuleiro = new Tabuleiro($scope.tamanho);
+            let tabuleiro = new Tabuleiro($scope.tamanho);
             tabuleiros = tabuleiro;
             $scope.tabuleiro = [];
-            var pecas = tabuleiro.getPecas();
-            $scope.tabuleiro = pecas;
+            $scope.tabuleiro = tabuleiro.getPecas();
             $scope.altura = '34px';
             $scope.peca1 = '';
             $scope.peca2 = '';
@@ -24,14 +25,14 @@ angular.module('app').controller('scripts',
                 $scope.altura = '34px';
                 if (!peca.selecionada && (peca.tipo !== 'casa' || $scope.peca1 !== '')) {
                     peca.selecionada = true;
-                    var qtdSelecionada = tabuleiro.getQtdSeleciondaENaoOcupada();
-                    if (qtdSelecionada == 1 && $scope.peca1 !== '') {
+                    let qtdSelecionada = tabuleiro.getQtdSeleciondaENaoOcupada();
+                    if (qtdSelecionada === 1 && $scope.peca1 !== '') {
                         console.log("Qtd Selecionada == 1 e $scope.peca1: " + $scope.peca1 + " !== ''");
                         peca.selecionada = true;
                         $scope.peca2 = peca;
-                        var peca1 = $scope.peca1;
-                        var peca2 = $scope.peca2;
-                        var estaoPerto = tabuleiro.pecasEstaoPerto(peca1, peca2);
+                        let peca1 = $scope.peca1;
+                        let peca2 = $scope.peca2;
+                        let estaoPerto = tabuleiro.pecasEstaoPerto(peca1, peca2);
                         console.log("Estão perto: " + estaoPerto);
                         if (peca1.ocupada && !peca2.ocupada) {
                             console.log("peca1 ocupada e peca2 não ocupada");
--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,10 @@
-var n = 0, ex = 0, w = 0, damasj1 = 0, damasj2 = 0;
+let n = 0, ex = 0, w = 0, damasj1 = 0, damasj2 = 0;
+
 function focar(peca) {
 //    console.log(peca.img);
-    var img = peca.img.split(',');
-    var i = img.length;
-    if (img[0] != 'url(img/pecas/espaco_casa.png)') {
+    let img = peca.img.split(',');
+    let i = img.length;
+    if (img[0] !== 'url(img/pecas/espaco_casa.png)') {
 //        console.log("ok10");
         peca.img = img[0];
     } else {
@@ -13,18 +14,19 @@ function focar(peca) {
 
     //    console.log("peca.img " + peca1.img);
 //    console.log(peca1.img);
-    for (var j = 1; j < i - 1; j++) {
+    for (let j = 1; j < i - 1; j++) {
 //        console.log("okf");
         peca.img += ',' + img[j];
 //        console.log(peca1.img);
     }
 
 }
+
 function focarOutros(pecas, peca) {
     this.tipo = peca.tipo;
-    var x = peca.pos.x;
-    var y = peca.pos.y;
-    if (this.tipo == "jogador1") {
+    let x = peca.pos.x;
+    let y = peca.pos.y;
+    if (this.tipo === "jogador1") {
         if (y + 1 <= n * 2 - 1) {
             if (x + 1 <= n * 2 - 1)
                 focar(pecas[y + 1][x + 1]);
@@ -32,7 +34,7 @@ function focarOutros(pecas, peca) {
                 focar(pecas[y + 1][x - 1]);
         }
     }
-    if (this.tipo == "jogador2") {
+    if (this.tipo === "jogador2") {
         if (y - 1 >= 0) {
             if (x + 1 <= n * 2 - 1)
                 focar(pecas[y - 1][x + 1]);
@@ -41,11 +43,15 @@ function focarOutros(pecas, peca) {
         }
     }
 }
-var n = 0, ex = 0, w = 0;
+
+n = 0;
+ex = 0;
+w = 0;
+
 function trocarPecas(peca, outraPeca) {
-    var aux = peca.img;
-    var aux2 = peca.tipo;
-    var aux3 = peca.ocupada;
+    let aux = peca.img;
+    let aux2 = peca.tipo;
+    let aux3 = peca.ocupada;
     peca.img = outraPeca.img;
     peca.tipo = outraPeca.tipo;
     peca.ocupada = outraPeca.ocupada;
@@ -56,73 +62,76 @@ function trocarPecas(peca, outraPeca) {
 
     console.log(peca.pos);
     console.log(outraPeca.pos);
-    if (peca.tipo === "jogador1" && peca.pos.x == n * 2 - 1) {
+    if (peca.tipo === "jogador1" && peca.pos.x === n * 2 - 1) {
         console.log("ok");
     }
 }
+
 function verificarDama(peca) {
-    var x = peca.pos.x;
-    var y = peca.pos.y;
-    if (peca.tipo == "jogador1" && y == n * 2 - 1)
-    {
+    let y = peca.pos.y;
+    if (peca.tipo === "jogador1" && y === n * 2 - 1) {
         peca.img = "url(img/pecas/black_queen_crown.png)," + peca.img;
         peca.tipo = "damaj1";
     }
-    if (peca.tipo == "jogador2" & y == 0)
-    {
+    if (peca.tipo === "jogador2" && y === 0) {
         peca.img = "url(img/pecas/white_queen_crown.png)," + peca.img;
         peca.tipo = "damaj2";
     }
 }
+
 function getAltura() {
-    if (w == 0) {
+    let btns = $("button");
+    if (w === 0) {
         ex = 1;
-        w = $("button").css('width');
+        w = btns.css('width');
         w = w.substring(0, w.indexOf('px'));
-        if (n == 5)
-            $("button").css('width', (w * 1.5) + "px");
-        else if (n == 4)
-            $("button").css('width', (w * 2) + "px");
-        $('button').css('height', $("button").css('width'));
-        w = $("button").css('width');
-        var h = $(".decimo").css('width');
+        if (n === 5)
+            btns.css('width', (w * 1.5) + "px");
+        else if (n === 4)
+            btns.css('width', (w * 2) + "px");
+        $('button').css('height', btns.css('width'));
+        w = btns.css('width');
+        let h = $(".decimo").css('width');
         h = h.substring(0, h.indexOf('px'));
         setLayout();
         return w;
     } else {
-        $("button").css('width', w);
-        $("button").css('height', w);
+        btns.css('width', w);
+        btns.css('height', w);
     }
 
 }
+
 function setLayout() {
-    var row = $('.row').css('width');
+    let clssRow = $('.row');
+    let row = clssRow.css('width');
     row = row.substring(0, row.indexOf('px'));
-    var tabuleiro = $('.coluna').css('width');
+    let tabuleiro = $('.coluna').css('width');
     tabuleiro = tabuleiro.substring(0, tabuleiro.indexOf('px'));
+    let btns = $('button');
     if (innerWidth <= 585 && innerWidth > 334) {
-        $('.row').css('margin', '0px !important');
-        $('button').css('width', (row / n / 2) + 'px');
-        $('button').css('height', (row / n / 2) + 'px');
+        clssRow.css('margin', '0px !important');
+        btns.css('width', (row / n / 2) + 'px');
+        btns.css('height', (row / n / 2) + 'px');
         console.log('ok4');
     } else if (innerWidth > 700) {
-        $('.row').css("margin-left", (((row - (tabuleiro * n * 2)) / 2) + 'px'));
-        $('.row').css("margin-rigth", (((row - (tabuleiro * n * 2)) / 2) + 'px'));
+        clssRow.css("margin-left", (((row - (tabuleiro * n * 2)) / 2) + 'px'));
+        clssRow.css("margin-rigth", (((row - (tabuleiro * n * 2)) / 2) + 'px'));
         console.log('ok3');
     } else if (row < tabuleiro * n * 2 && innerWidth > 334) {
-        $('button').css('width', (row / n / 2) + 'px');
-        $('button').css('height', (row / n / 2) + 'px');
+        btns.css('width', (row / n / 2) + 'px');
+        btns.css('height', (row / n / 2) + 'px');
         console.log('ok2');
-    } else if (innerWidth == 320) {
+    } else if (innerWidth === 320) {
         w = (innerWidth / 8) * 0.9;
         $('.container-fluid').css('width', (innerWidth) + 'px');
         $('.jumbotron').css('width', "-webkit-fill-available")
-                .css('margin', '0px')
-                .css('margin-top', '10%')
-                .css('padding', '0px');
-        $('.row').css('width', (innerWidth) + 'px');
-        $('button').css('width', ((innerWidth / 8) * 0.9) + 'px');
-        $('button').css('height', ((innerWidth / 8) * 0.9) + 'px');
+            .css('margin', '0px')
+            .css('margin-top', '10%')
+            .css('padding', '0px');
+        clssRow.css('width', (innerWidth) + 'px');
+        btns.css('width', ((innerWidth / 8) * 0.9) + 'px');
+        btns.css('height', ((innerWidth / 8) * 0.9) + 'px');
 
         console.log('ok');
     }
@@ -137,48 +146,55 @@ function setLayout() {
     console.log('row ' + row);
     console.log('page ' + innerWidth);
 }
-class  Tabuleiro {
+
+class Tabuleiro {
     constructor(tamanho) {
         this.tamanho = tamanho;
         n = this.tamanho;
         this.pecas = [];
-        for (var i = 0; i < this.tamanho; i++) {
+        for (let i = 0; i < this.tamanho; i++) {
             this.pecas[i] = [];
         }
         this.preencherPecas();
     }
-    setImagePecas() { }
+
+    setImagePecas() {
+    }
+
     getPecas() {
         return this.pecas;
     }
+
     setPecas(pecas) {
         this.pecas = pecas;
     }
+
     preencherPecas() {
-        for (var i = 0; i < this.tamanho * 2; i++) {
-            var colunas = [];
-            for (var j = 0; j < this.tamanho * 2; j++) {
-                var img, ocupada = false, tipo = 'vazio';
-                if ((i % 2 == 0 & j % 2 == 0) || (i % 2 != 0 & j % 2 != 0)) {
+        for (let i = 0; i < this.tamanho * 2; i++) {
+            let colunas = [];
+            for (let j = 0; j < this.tamanho * 2; j++) {
+                let img, ocupada = false, tipo = 'vazio';
+                if ((i % 2 === 0 & j % 2 === 0) || (i % 2 !== 0 & j % 2 !== 0)) {
                     if (i <= n - 2) {
                         img = ''
-                                //+'url(img/pecas/black_queen_crown.png),'
-                                + 'url(img/pecas/black_man2.png)'
-                                + ',url(img/pecas/espaco_casa.png)';
+                            //+'url(img/pecas/black_queen_crown.png),'
+                            + 'url(img/pecas/black_man2.png)'
+                            + ',url(img/pecas/espaco_casa.png)';
                         ocupada = true;
                         tipo = 'jogador1';
                     } else if (i > n) {
                         img = ''
-                                //+'url(img/pecas/white_queen_crown.png),'
-                                + 'url(img/pecas/white_man2.png)'
-                                + ',url(img/pecas/espaco_casa.png)';
+                            //+'url(img/pecas/white_queen_crown.png),'
+                            + 'url(img/pecas/white_man2.png)'
+                            + ',url(img/pecas/espaco_casa.png)';
                         tipo = 'jogador2';
                         ocupada = true;
                     } else {
                         img = 'url(img/pecas/espaco_casa.png)';
                         tipo = 'casa';
                     }
-                    colunas[j] = {id: i + '-' + j,
+                    colunas[j] = {
+                        id: i + '-' + j,
                         pos: {
                             x: j,
                             y: i
@@ -191,7 +207,8 @@ class  Tabuleiro {
                     }
                 } else {
                     ocupada = true;
-                    colunas[j] = {id: '-' + i + '-' + j,
+                    colunas[j] = {
+                        id: '-' + i + '-' + j,
                         pos: {
                             x: j,
                             y: i
@@ -207,83 +224,87 @@ class  Tabuleiro {
             this.pecas[i] = colunas;
         }
     }
+
     getQtdSeleciondaENaoOcupada(peca) {
-        var qtdSelecionada = 0;
-        for (var i = 0; i < this.tamanho * 2; i++) {
-            for (var j = 0; j < this.tamanho * 2; j++) {
+        let qtdSelecionada = 0;
+        for (let i = 0; i < this.tamanho * 2; i++) {
+            for (let j = 0; j < this.tamanho * 2; j++) {
                 if (this.pecas[i][j].selecionada &&
-                        this.pecas[i][j].tipo !== 'vazio' && this.pecas[i][j] !== peca) {
+                    this.pecas[i][j].tipo !== 'vazio' && this.pecas[i][j] !== peca) {
                     qtdSelecionada++;
                 }
             }
         }
         return qtdSelecionada;
     }
+
     getPecaSelecionda(peca) {
-        for (var i = 0; i < this.tamanho * 2; i++) {
-            for (var j = 0; j < this.tamanho * 2; j++) {
+        for (let i = 0; i < this.tamanho * 2; i++) {
+            for (let j = 0; j < this.tamanho * 2; j++) {
                 if (this.pecas[i][j].selecionada && this.pecas[i][j] !== peca) {
-                    return  this.pecas[i][j];
+                    return this.pecas[i][j];
                 }
             }
         }
     }
+
     pecasEstaoPerto(peca1, peca2) {
-        var id1 = peca1.id.split('-');
-        var id2 = peca2.id.split('-');
+        let id1 = peca1.id.split('-');
+        let id2 = peca2.id.split('-');
         id1[0] = parseInt(id1[0]);
         id1[1] = parseInt(id1[1]);
         id2[0] = parseInt(id2[0]);
         id2[1] = parseInt(id2[1]);
-        var peca1PodeMoverCima = (peca1.tipo === 'jogador1' ? false : true);
-        var peca1PodeMoverBaixo = (peca1.tipo === 'jogador2' ? false : true);
-        var estaoPerto = (((id1[0] + 1 == id2[0] && peca1PodeMoverBaixo) //linha abaixo
-                || (id1[0] - 1 == id2[0] && peca1PodeMoverCima))//linha acima
-                && ((id1[1] + 1 == id2[1])//coluna direita
-                        || (id1[1] - 1 == id2[1]))); //coluna esquerda
+        let peca1PodeMoverCima = peca1.tipo !== 'jogador1';
+        let peca1PodeMoverBaixo = peca1.tipo !== 'jogador2';
+        let estaoPerto = (((id1[0] + 1 === id2[0] && peca1PodeMoverBaixo) //linha abaixo
+            || (id1[0] - 1 === id2[0] && peca1PodeMoverCima))//linha acima
+            && ((id1[1] + 1 === id2[1])//coluna direita
+                || (id1[1] - 1 === id2[1]))); //coluna esquerda
         return estaoPerto;
     }
+
     EPossivelComer(pecas, peca1, peca2) {
-        var id1 = peca1.id.split('-');
-        var id2 = peca2.id.split('-');
+        let id1 = peca1.id.split('-');
+        let id2 = peca2.id.split('-');
         id1[0] = parseInt(id1[0]);
         id1[1] = parseInt(id1[1]);
         id2[0] = parseInt(id2[0]);
         id2[1] = parseInt(id2[1]);
-        var peca1PodeMoverCima = (peca1.tipo === 'jogador1' ? false : true);
-        var peca1PodeMoverBaixo = (peca1.tipo === 'jogador2' ? false : true);
+        let peca1PodeMoverCima = (peca1.tipo !== 'jogador1');
+        let peca1PodeMoverBaixo = (peca1.tipo !== 'jogador2');
         //Abaixo a direita
-        var possivelPecaASerComida_b_d = 0;
+        let possivelPecaASerComida_b_d = 0;
         try {
             possivelPecaASerComida_b_d = pecas[id1[0] + 1][id1[1] + 1];
             //Abaixo a esquerda
         } catch (err) {
         }
-        var possivelPecaASerComida_b_e = 0;
+        let possivelPecaASerComida_b_e = 0;
         try {
             possivelPecaASerComida_b_e = pecas[id1[0] + 1][id1[1] - 1];
         } catch (err) {
         }
 //Acima a direita
-        var possivelPecaASerComida_c_d = 0;
+        let possivelPecaASerComida_c_d = 0;
         try {
             possivelPecaASerComida_c_d = pecas[id1[0] - 1][id1[1] + 1];
         } catch (err) {
         }
 //Acima a esquerda
-        var possivelPecaASerComida_c_e = 0;
+        let possivelPecaASerComida_c_e = 0;
         try {
             possivelPecaASerComida_c_e = pecas[id1[0] - 1][id1[1] - 1];
         } catch (err) {
         }
         this.resetPecas();
-        var res = false;
+        let res = false;
         try {
             if (possivelPecaASerComida_b_d.ocupada &&
-                    (id1[0] + 2 == id2[0])
-                    && (id1[1] + 2 == id2[1])
-                    && peca1PodeMoverBaixo
-                    && peca1.tipo !== pecas[id1[0] + 1][id1[1] + 1].tipo) {
+                (id1[0] + 2 === id2[0])
+                && (id1[1] + 2 === id2[1])
+                && peca1PodeMoverBaixo
+                && peca1.tipo !== pecas[id1[0] + 1][id1[1] + 1].tipo) {
                 possivelPecaASerComida_b_d.cor = 'primary';
                 possivelPecaASerComida_b_d.img = 'url(img/pecas/espaco_casa.png)';
                 possivelPecaASerComida_b_d.ocupada = false;
@@ -292,11 +313,9 @@ class  Tabuleiro {
         } catch (err) {
         }
         try {
-            if (possivelPecaASerComida_b_e.ocupada &&
-                    (id1[0] + 2 == id2[0])
-                    && (id1[1] - 2 == id2[1])
-                    && peca1PodeMoverBaixo
-                    && peca1.tipo !== pecas[id1[0] + 1][id1[1] - 1].tipo) {
+            if (possivelPecaASerComida_b_e.ocupada && peca1PodeMoverBaixo
+                && (id1[0] + 2 === id2[0]) && (id1[1] - 2 === id2[1])
+                && peca1.tipo !== pecas[id1[0] + 1][id1[1] - 1].tipo) {
                 possivelPecaASerComida_b_e.cor = 'primary';
                 possivelPecaASerComida_b_e.img = 'url(img/pecas/espaco_casa.png)';
                 possivelPecaASerComida_b_e.ocupada = false;
@@ -306,10 +325,10 @@ class  Tabuleiro {
         }
         try {
             if (possivelPecaASerComida_c_d.ocupada &&
-                    (id1[0] - 2 == id2[0])
-                    && (id1[1] + 2 == id2[1])
-                    && peca1PodeMoverCima
-                    && peca1.tipo !== pecas[id1[0] - 1][id1[1] + 1].tipo) {
+                (id1[0] - 2 === id2[0])
+                && (id1[1] + 2 === id2[1])
+                && peca1PodeMoverCima
+                && peca1.tipo !== pecas[id1[0] - 1][id1[1] + 1].tipo) {
                 possivelPecaASerComida_c_d.cor = 'primary';
                 possivelPecaASerComida_c_d.img = 'url(img/pecas/espaco_casa.png)';
                 possivelPecaASerComida_c_d.ocupada = false;
@@ -319,10 +338,10 @@ class  Tabuleiro {
         }
         try {
             if (possivelPecaASerComida_c_e.ocupada &&
-                    (id1[0] - 2 == id2[0])
-                    && (id1[1] - 2 == id2[1])
-                    && peca1PodeMoverCima
-                    && peca1.tipo !== pecas[id1[0] - 1][id1[1] - 1].tipo) {
+                (id1[0] - 2 === id2[0])
+                && (id1[1] - 2 === id2[1])
+                && peca1PodeMoverCima
+                && peca1.tipo !== pecas[id1[0] - 1][id1[1] - 1].tipo) {
                 possivelPecaASerComida_c_e.cor = 'primary';
                 possivelPecaASerComida_c_e.img = 'url(img/pecas/espaco_casa.png)';
                 possivelPecaASerComida_c_e.ocupada = false;
@@ -332,9 +351,10 @@ class  Tabuleiro {
         }
         return res;
     }
+
     resetPecas() {
-        for (var i = 0; i < this.tamanho * 2; i++) {
-            for (var j = 0; j < this.tamanho * 2; j++) {
+        for (let i = 0; i < this.tamanho * 2; i++) {
+            for (let j = 0; j < this.tamanho * 2; j++) {
                 this.pecas[i][j].selecionada = false;
             }
         }
@@ -342,17 +362,17 @@ class  Tabuleiro {
     }
 
     desfocar() {
-        var pecas = this.pecas;
-        for (var i = 0; i < n * 2; i++) {
-            for (var j = 0; j < n * 2; j++) {
-                if (pecas[i][j].img == '') {
+        let pecas = this.pecas;
+        for (let i = 0; i < n * 2; i++) {
+            for (let j = 0; j < n * 2; j++) {
+                if (pecas[i][j].img === '') {
 //                    console.log(pecas[i][j].img);
                     console.log("11111111111111111");
                     pecas[i][j].img = 'url(img/pecas/espaco_casa.png)';
-                } else if (pecas[i][j].img.indexOf("casa") != -1) {
+                } else if (pecas[i][j].img.indexOf("casa") !== -1) {
                     console.log("22222222222222");
                     pecas[i][j].img += ',url(img/pecas/espaco_casa.png)';
-                } else if (pecas[i][j].img.indexOf("url(img/pecas/espaco_vazio.png)") == -1) {
+                } else if (pecas[i][j].img.indexOf("url(img/pecas/espaco_vazio.png)") === -1) {
                     console.log("llllllllllllllll");
                     console.log("Esse não alterou");
                     console.log(i + " " + j);
