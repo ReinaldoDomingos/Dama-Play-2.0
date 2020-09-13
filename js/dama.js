9cbd5c690659f336a94b5303b20f395d9f555830
ReinaldoDomingos <reijunior100@gmail.com>
2020-09-12 21:10:28 -0400

    Movimentação de peças simples funcionando

--- js/dama.js
+++ js/dama.js
@@ -7,7 +7,13 @@ function moverPeca($scope, peca) {
     let campoPeca = $scope.tabuleiro[x1][y1];
     let campoDesocupado = $scope.tabuleiro[x2][y2];
 
-    if (!podeMoverPeca($scope, x1, x2, y1, y2, campoPeca.jogador)) return;
+    let podeMover = podeMoverPeca($scope, x1, x2, y1, y2, campoPeca.jogador);
+    if (!podeMover) {
+        return
+    } else if (podeMover === 2) {
+        let isMovimentoDuplo = movimentoDuplo($scope, x1, x2, y1, y2, campoPeca.jogador);
+        if (!isMovimentoDuplo) return;
+    }
 
     console.log(x1, y1, ' para ', x2, y2);
     campoPeca.pos = {x: x2, y: y2};
@@ -22,31 +28,51 @@ function moverPeca($scope, peca) {
 
 function removerPeca($scope, x, y) {
     $scope.tabuleiro[x][y] = addCampoSemPeca(idAtual++);
+    $scope.tabuleiro[x][y].pos = {x: x, y: y};
 }
 
 function movimentoDuplo($scope, x1, x2, y1, y2, jogador) {
     console.log('movimentoDuplo', `${x1}x${y1} para ${x2}x${y2}`);
-    if (jogador === 2 && x1 - x2 === 2 && (y1 - y2 === 2 || y2 - y1 === 2)) {
+    if (jogador === 2 && x1 - x2 === 2) {
         console.log('mov1')
-        if (y1 > 0 && $scope.tabuleiro[x1 - 1][y1 - 1].jogador === 1) {
+        if (y1 > 0 && y1 - y2 === 2 && $scope.tabuleiro[x1 - 1][y1 - 1].jogador === 1) {
             console.log('capturar peça jogador 1');
             removerPeca($scope, x1 - 1, y1 - 1);
             return true;
         }
-        if (y2 < 8 && $scope.tabuleiro[x1 - 1][y1 + 1].jogador === 1) {
+        if (y2 < 8 && y2 - y1 === 2 && $scope.tabuleiro[x1 - 1][y1 + 1].jogador === 1) {
             console.log('capturar peça jogador 1');
             removerPeca($scope, x1 - 1, y1 + 1);
             return true;
         }
     }
+
+    if (jogador === 1 && x2 - x1 === 2 && (y1 - y2 === 2 || y2 - y1 === 2)) {
+        console.log('mov2')
+        if (y1 > 0 && $scope.tabuleiro[x2 - 1][y1 - 1].jogador === 2) {
+            console.log('capturar peça jogador 1');
+            removerPeca($scope, x2 - 1, y1 - 1);
+            return true;
+        }
+        if (y2 < 8 && $scope.tabuleiro[x2 - 1][y1 + 1].jogador === 2) {
+            console.log('capturar peça jogador 1');
+            removerPeca($scope, x2 - 1, y1 + 1);
+            return true;
+        }
+    }
     console.log('não é movimentoDuplo');
+    return false;
 }
 
 function podeMoverPeca($scope, x1, x2, y1, y2, jogador) {
-    if ((y2 - y1 === 1 || y1 - y2 === 1) && ((jogador === 1 && x2 - x1 === 1) || (jogador === 2 && x1 - x2 === 1))) {
+    if (y1 === y2) {
+        return false;
+    } else if ((y2 - y1 === 1 || y1 - y2 === 1) && ((jogador === 1 && x2 - x1 === 1) || (jogador === 2 && x1 - x2 === 1))) {
         console.log('podeMoverPeca', `${x1}x${y1} para ${x2}x${y2}`);
         return true;
     } else if ((jogador === 1 && x2 - x1 === 2) || (jogador === 2 && x1 - x2 === 2)) {
-        return movimentoDuplo($scope, x1, x2, y1, y2, jogador);
+        return 2;
     }
+
+    return false;
 }
