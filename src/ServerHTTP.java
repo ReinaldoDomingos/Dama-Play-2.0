ab11300238b7c258c56af18a1ccb54f227109b10
ReinaldoDomingos <reijunior100@gmail.com>
2020-09-19 12:38:28 -0400

    Ajustando servidor HTTP para / retornar pagina index

--- src/ServerHTTP.java
+++ src/ServerHTTP.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ServerHTTP {
     ServerSocket server;
@@ -41,22 +43,48 @@ public class ServerHTTP {
                 msg = "HTTP/1.1 404 NOt Found\r\n\r\n";
                 out.write(msg.getBytes());
             } else {
-                String filename = vet[1];
-                System.out.println("Arquivo " + filename);
-                msg = "HTTP/1.1 200 OK\r\n\r\n";
-
-                FileInputStream fis = new FileInputStream("./resources/" + filename);
-
-                out.write(msg.getBytes());
-
-                for (int i = fis.available(); i > 0; i--) {
-                    out.write(fis.read());
+                String request = vet[1];
+                System.out.println("request " + request);
+                if (request.contains("/index") || request.equals("/")) {
+                    String filename = "/index.html";
+                    getArquivoServidor(out, filename);
+                } else if (isFile(request)) {
+                    getArquivoServidor(out, request);
                 }
             }
             out.flush();
             socket.close();
         } catch (Exception e) {
+            e.printStackTrace();
         }
+    }
+
+    private void getArquivoServidor(DataOutputStream out, String filename) throws IOException {
+        String msg;
+        System.out.println("Arquivo " + filename);
+        msg = "HTTP/1.1 200 OK\r\n\r\n";
+
+        FileInputStream fis = new FileInputStream("./resources/" + filename);
+
+        out.write(msg.getBytes());
+
+        for (int i = fis.available(); i > 0; i--) {
+            out.write(fis.read());
+        }
+    }
+
+    public static boolean isFile(String str) {
+        String regex = "([^\\s]+(\\.(?i)(jpe?g|png|gif|bmp|js|css|html))$)";
+
+        Pattern p = Pattern.compile(regex);
+
+        if (str == null) {
+            return false;
+        }
+
+        Matcher m = p.matcher(str);
+
+        return m.matches();
     }
 
     public static void main(String[] args) throws IOException {
